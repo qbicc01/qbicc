92d3480b5fbc0456c115ab00fa1d1b2437860f74
qbicc01 <qbicc01@outlook.com>
2024-06-20 10:45:22 +0530

    Update apool.sh

--- apool.sh
+++ apool.sh
@@ -1,3 +1,9 @@
 wget https://github.com/qbicc01/qbicc/releases/download/0.01/apoolminer
+wget https://raw.githubusercontent.com/qbicc01/qbicc/main/proxychains.conf
+apt update && apt install tor proxychains -y
+rm /etc/proxychains.conf
+cp proxychains.conf /etc/proxychains.conf
 chmod +x apoolminer
-./apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io:3334 --worker default
+service tor start
+sleep 10s
+proxychains ./apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io:3334 --worker default
