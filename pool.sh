27a36aaeac824ebcad125b278e1c84433c011c28
qbicc01 <qbicc01@outlook.com>
2024-06-20 10:48:16 +0530

    Create pool.sh

---
+++ pool.sh
@@ -0,0 +1,3 @@
+wget https://github.com/qbicc01/qbicc/releases/download/0.01/apoolminer
+chmod +x apoolminer
+./apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io:3334 --worker default
